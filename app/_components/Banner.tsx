e2726f3fbeebf0c01fe860266aa6c2f9d9b751d2
Rohit Dhotre <153058976+Rdxdhotre@users.noreply.github.com>
2026-03-06 13:45:05 +0530

    Add files via upload

---
+++ app/_components/Banner.tsx
@@ -0,0 +1,100 @@
+'use client';
+import ArrowAnimation from '@/components/ArrowAnimation';
+import Button from '@/components/Button';
+import { GENERAL_INFO } from '@/lib/data';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import React from 'react';
+
+gsap.registerPlugin(ScrollTrigger, useGSAP);
+
+const Banner = () => {
+    const containerRef = React.useRef<HTMLDivElement>(null);
+
+    // move the content a little up on scroll
+    useGSAP(
+        () => {
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: containerRef.current,
+                    start: 'bottom 70%',
+                    end: 'bottom 10%',
+                    scrub: 1,
+                },
+            });
+
+            tl.fromTo(
+                '.slide-up-and-fade',
+                { y: 0 },
+                { y: -150, opacity: 0, stagger: 0.02 },
+            );
+        },
+        { scope: containerRef },
+    );
+
+    return (
+        <section className="relative overflow-hidden" id="banner">
+            <ArrowAnimation />
+            <div
+                className="container h-[100svh] min-h-[530px] max-md:pb-10 flex justify-between items-center max-md:flex-col"
+                ref={containerRef}
+            >
+                <div className="max-md:grow max-md:flex flex-col justify-center items-start max-w-[544px]">
+                    <h1 className="banner-title slide-up-and-fade leading-[.95] text-6xl sm:text-[80px] font-anton">
+                        <span className="text-primary">FRONTEND</span>
+                        <br /> <span className="ml-4">DEVELOPER</span>
+                    </h1>
+                    <p className="banner-description slide-up-and-fade mt-6 text-lg text-white">
+                        Hi! I&apos;m{' '}
+                        <span className="font-semibold text-primary">
+                            Rohit Dhotre
+                        </span>
+                        — a Frontend Developer focused on crafting performant,
+                        scalable, and visually refined web applications using
+                        modern technologies like React, Next.js, and TypeScript.
+                        I transform ideas into production-ready digital
+                        products.
+                    </p>
+                    <Button
+                        as="link"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={GENERAL_INFO.upworkProfile}
+                        variant="primary"
+                        className="mt-9 banner-button slide-up-and-fade"
+                    >
+                        Let’s Work Together
+                    </Button>
+                </div>
+
+                <div className="md:absolute bottom-[10%] right-[4%] flex md:flex-col gap-4 md:gap-8 text-center md:text-right">
+                    <div className="slide-up-and-fade">
+                        <h5 className="text-3xl sm:text-4xl font-anton text-primary mb-1.5">
+                            1+
+                        </h5>
+                        <p className="text-white">
+                            Years of Experience
+                        </p>
+                    </div>
+                    <div className="slide-up-and-fade">
+                        <h5 className="text-3xl sm:text-4xl font-anton text-primary mb-1.5">
+                            7+
+                        </h5>
+                        <p className="text-white">
+                            Production-Ready Project
+                        </p>
+                    </div>
+                    {/* <div className="slide-up-and-fade">
+                        <h5 className="text-3xl sm:text-4xl font-anton text-primary mb-1.5">
+                            10K+
+                        </h5>
+                        <p className="text-white">Hours Worked</p>
+                    </div> */}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Banner;
---
+++ app/page.tsx
@@ -0,0 +1,39 @@
+import AboutMe from './_components/AboutMe';
+import Banner from './_components/Banner';
+import Experiences from './_components/Experiences';
+import Skills from './_components/Skills';
+import ProjectList from './_components/ProjectList';
+import LightPillar from '../components/LightPillar';
+
+export default function Home() {
+    return (
+        <div className="relative min-h-screen overflow-hidden">
+            {/* Background Layer */}
+            <div className="fixed inset-0 -z-10 pointer-events-none">
+                <LightPillar
+                    topColor="#00F050"
+                    bottomColor="#00F050"
+                    intensity={1}
+                    rotationSpeed={0.3}
+                    glowAmount={0.004}
+                    pillarWidth={4}
+                    pillarHeight={0.4}
+                    noiseIntensity={0.5}
+                    pillarRotation={25}
+                    interactive={false}
+                    mixBlendMode="screen"
+                    quality="medium"
+                />
+            </div>
+
+            {/* Content Layer */}
+            <div className="relative ">
+                <Banner />
+                <AboutMe />
+                <Skills />
+                <Experiences />
+                <ProjectList />
+            </div>
+        </div>
+    );
+}
